85cf7949c253a92986c81238042e25d016a4e13b
Chase McCarthy <charles.mccarthy@heroku.com>
2019-11-20 11:59:04 -0600

    chore: Migrate from TSLint to ESLint (#11)
    * chore: convert to eslint
    
    * chore: fix eslint issues
    
    * chore(deps): use correct @types/node for node 8

--- test/commands/commands.test.ts
+++ test/commands/commands.test.ts
@@ -2,16 +2,16 @@ import {expect, test} from '@oclif/test'
 
 describe('commands', () => {
   test
-    .stdout()
-    .command(['commands'])
-    .it('runs commands', ctx => {
-      expect(ctx.stdout).to.equal('commands\nhelp\n')
-    })
+  .stdout()
+  .command(['commands'])
+  .it('runs commands', ctx => {
+    expect(ctx.stdout).to.equal('commands\nhelp\n')
+  })
 
   test
-    .stdout()
-    .command(['commands', '-j'])
-    .it('runs commands -j', ctx => {
-      expect(JSON.parse(ctx.stdout)[0].id).to.equal('commands')
-    })
+  .stdout()
+  .command(['commands', '-j'])
+  .it('runs commands -j', ctx => {
+    expect(JSON.parse(ctx.stdout)[0].id).to.equal('commands')
+  })
 })
