4469827d69f12d404d8a93f42b44fff9bde38bdb
Thomas Dvornik <amphro@gmail.com>
2020-06-01 11:58:16 -0700

    feat: output commands in cli-ux table (#14)

--- src/commands/commands.ts
+++ src/commands/commands.ts
@@ -1,29 +1,123 @@
 import {Command, flags} from '@oclif/command'
-import ux from 'cli-ux'
+import {ux} from 'cli-ux'
 import * as _ from 'lodash'
+import {EOL} from 'os'
 
+type Dictionary = {[index: string]: object}
 export default class Commands extends Command {
   static description = 'list all the commands'
 
-  static flags = {
+  static flags: flags.Input<any> = {
     help: flags.help({char: 'h'}),
-    json: flags.boolean({char: 'j', description: 'output in json format'}),
-    hidden: flags.boolean({description: 'also show hidden commands'}),
+    json: flags.boolean({char: 'j', description: 'display unfiltered api data in json format'}),
+    hidden: flags.boolean({description: 'show hidden commands'}),
+    ...ux.table.flags(),
   }
 
   async run() {
     const {flags} = this.parse(Commands)
-    let commands = this.config.commands
+    let commands = this.getCommands()
     if (!flags.hidden) {
       commands = commands.filter(c => !c.hidden)
     }
-    commands = _.sortBy(commands, 'id')
+
+    const config = this.config
+    commands = _.sortBy(commands, 'id').map(command => {
+      // Template supported fields.
+      command.description = (typeof command.description === 'string' && _.template(command.description)({command, config})) || undefined
+      command.usage = (typeof command.usage === 'string' && _.template(command.usage)({command, config})) || undefined
+      return command
+    })
+
     if (flags.json) {
-      ux.styledJSON(commands)
+      ux.styledJSON(commands.map(cmd => {
+        let commandClass = cmd.load()
+        const obj = Object.assign({}, cmd, commandClass)
+
+        // Load all properties on all extending classes.
+        while (commandClass !== undefined) {
+          commandClass = Object.getPrototypeOf(commandClass) || undefined
+          Object.assign(obj, commandClass)
+        }
+
+        // The plugin property on the loaded class contains a LOT of information including all the commands again. Remove it.
+        delete obj.plugin
+
+        // If Command classes have circular references, don't break the commands command.
+        return this.removeCycles(obj)
+      }))
     } else {
-      for (const c of commands) {
-        this.log(c.id)
-      }
+      ux.table(commands.map(command => {
+        // Massage some fields so it looks good in the table
+        command.description = (command.description || '').split(EOL)[0]
+        command.hidden = Boolean(command.hidden)
+        command.usage = (command.usage || '')
+        return command
+      }), {
+        id: {
+          header: 'Command',
+        },
+        description: {},
+        usage: {
+          extended: true,
+        },
+        pluginName: {
+          extended: true,
+          header: 'Plugin',
+        },
+        pluginType: {
+          extended: true,
+          header: 'Type',
+        },
+        hidden: {
+          extended: true,
+        },
+      }, {
+        printLine: this.log,
+        ...flags, // parsed flags
+      })
     }
+  }
+
+  private getCommands() {
+    return this.config.commands
+  }
+
+  private removeCycles(object: unknown) {
+    // Keep track of seen objects.
+    const seenObjects = new WeakMap<Dictionary, undefined>()
+
+    const _removeCycles = (obj: unknown) => {
+      // Use object prototype to get around type and null checks
+      if (Object.prototype.toString.call(obj) === '[object Object]') {
+        // We know it is a "Dictionary" because of the conditional
+        const dictionary = obj as Dictionary
+
+        if (seenObjects.has(dictionary)) {
+          // Seen, return undefined to remove.
+          return undefined
+        }
+
+        seenObjects.set(dictionary, undefined)
+
+        for (const key in dictionary) {
+          // Delete the duplicate object if cycle found.
+          if (_removeCycles(dictionary[key]) === undefined) {
+            delete dictionary[key]
+          }
+        }
+      } else if (Array.isArray(obj)) {
+        for (const i in obj) {
+          if (_removeCycles(obj[i]) === undefined) {
+            // We don't want to delete the array, but we can replace the element with null.
+            obj[i] = null
+          }
+        }
+      }
+
+      return obj
+    }
+
+    return _removeCycles(object)
   }
 }
--- test/commands/commands.test.ts
+++ test/commands/commands.test.ts
@@ -1,17 +1,159 @@
 import {expect, test} from '@oclif/test'
+import {Command} from '@oclif/command'
+import Commands from '../../src/commands/commands'
+
+abstract class TestCommand extends Command {
+  public static testCustomProperty = 'test'
+}
+
+const obj: {[index: string]: {}} = {}
+obj.circular = obj
+
+class AnotherTestCommand extends TestCommand {
+  public static circularObj = obj
+
+  public static anotherCustomProperty = [5, obj]
+
+  public async run() {
+    // Do nothing
+  }
+}
+
+const commandList = [{
+  id: 'topic:subtopic:command',
+  description: 'super good test command',
+  usage: 'topic:subtopic:command --json',
+  pluginName: 'test',
+  pluginType: 'core',
+  aliases: [],
+  flags: {},
+  load: () => TestCommand,
+}, {
+  id: 'anothertopic:subtopic:command',
+  description: 'another super good test command',
+  usage: 'anothertopic:subtopic:command --json',
+  pluginName: 'anothertest',
+  pluginType: 'core',
+  aliases: [],
+  flags: {},
+  load: () => AnotherTestCommand,
+}, {
+  id: 'hidden',
+  hidden: true,
+}]
 
 describe('commands', () => {
   test
   .stdout()
   .command(['commands'])
-  .it('runs commands', ctx => {
-    expect(ctx.stdout).to.equal('commands\nhelp\n')
+  .it('runs commands', (ctx: { stdout: any }) => {
+    expect(ctx.stdout).to.equal(
+      'Command  Description                    \n' +
+      'commands list all the commands          \n' +
+      'help     display help for oclif-example \n',
+    )
   })
 
   test
   .stdout()
   .command(['commands', '-j'])
-  .it('runs commands -j', ctx => {
+  .it('runs commands -j', (ctx: { stdout: string }) => {
     expect(JSON.parse(ctx.stdout)[0].id).to.equal('commands')
+  })
+
+  test
+  .stub(Commands.prototype, 'getCommands', () => commandList)
+  .stdout()
+  .command(['commands', '--hidden'])
+  .it('runs commands --hidden', (ctx: { stdout: any }) => {
+    expect(ctx.stdout).to.equal(
+      'Command                       Description                     \n' +
+      'anothertopic:subtopic:command another super good test command \n' +
+      'hidden                                                        \n' +
+      'topic:subtopic:command        super good test command         \n',
+    )
+  })
+
+  test
+  .stub(Commands.prototype, 'getCommands', () => commandList)
+  .stdout()
+  .command(['commands', '--filter=Command=^topic'])
+  .it('runs commands --filter="Command=^topic"', (ctx: { stdout: any }) => {
+    expect(ctx.stdout).to.equal(
+      'Command                Description             \n' +
+      'topic:subtopic:command super good test command \n',
+    )
+  })
+
+  test
+  .stub(Commands.prototype, 'getCommands', () => commandList)
+  .stdout()
+  .command(['commands', '--filter=Plugin=anothertest'])
+  .it('runs commands --filter="Plugin=anothertest"', (ctx: { stdout: any }) => {
+    expect(ctx.stdout).to.equal(
+      'Command                       Description                     \n' +
+      'anothertopic:subtopic:command another super good test command \n',
+    )
+  })
+
+  test
+  .stub(Commands.prototype, 'getCommands', () => commandList)
+  .stdout()
+  .command(['commands', '--filter=Command=anothertopic:subtopic:command'])
+  .it('runs commands --filter="Command=anothertopic:subtopic:command"', (ctx: { stdout: any }) => {
+    expect(ctx.stdout).to.equal(
+      'Command                       Description                     \n' +
+      'anothertopic:subtopic:command another super good test command \n',
+    )
+  })
+
+  test
+  .stub(Commands.prototype, 'getCommands', () => commandList)
+  .stdout()
+  .command(['commands', '--columns=Command', '--no-header'])
+  .it('runs commands --filter=Command=', (ctx: { stdout: any }) => {
+    expect(ctx.stdout).to.equal(
+      'anothertopic:subtopic:command \n' +
+      'topic:subtopic:command        \n',
+    )
+  })
+
+  test
+  .stub(Commands.prototype, 'getCommands', () => commandList)
+  .stdout()
+  .command(['commands', '--filter=Command=subtopic:command'])
+  .it('runs commands --filter"=Command=subtopic:command"', (ctx: { stdout: any }) => {
+    expect(ctx.stdout).to.equal(
+      'Command                       Description                     \n' +
+      'anothertopic:subtopic:command another super good test command \n' +
+      'topic:subtopic:command        super good test command         \n',
+    )
+  })
+
+  test
+  .stub(Commands.prototype, 'getCommands', () => commandList)
+  .stdout()
+  .command(['commands', '--filter=Command=^topic:subtopic:command'])
+  .it('runs commands --filter"=Command=^topic:subtopic:command"', (ctx: { stdout: any }) => {
+    expect(ctx.stdout).to.equal(
+      'Command                Description             \n' +
+      'topic:subtopic:command super good test command \n',
+    )
+  })
+
+  test
+  .stub(Commands.prototype, 'getCommands', () => commandList)
+  .stdout()
+  .command(['commands', '--json'])
+  .it('runs commands --json', (ctx: { stdout: string }) => {
+    const commands = JSON.parse(ctx.stdout)
+    expect(commands[0].id).to.equal('anothertopic:subtopic:command')
+    expect(commands[0].testCustomProperty).to.equal('test')
+    expect(commands[0].anotherCustomProperty[0]).to.equal(5)
+    expect(commands[0].anotherCustomProperty[1]).to.equal(null)
+    expect(commands[0].circularObj.circular).to.equal(undefined)
+    expect(commands[1].id).to.equal('topic:subtopic:command')
+    expect(commands[1].testCustomProperty).to.equal('test')
+    expect(commands[1].anotherCustomProperty).to.equal(undefined)
   })
 })
