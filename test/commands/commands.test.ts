398eb40668bbbe2af1f05c0d4c5bc97845cc6dbf
Cristian Dominguez <cdominguez@salesforce.com>
2024-05-21 14:22:25 -0300

    test: migrate UTs to oclif/test v4

--- test/commands/commands.test.ts
+++ test/commands/commands.test.ts
@@ -1,5 +1,7 @@
 import {Command} from '@oclif/core'
-import {expect, test} from '@oclif/test'
+import {runCommand} from '@oclif/test'
+import {expect} from 'chai'
+import {SinonSandbox, createSandbox} from 'sinon'
 
 import Commands from '../../src/commands/commands.js'
 
@@ -75,140 +77,139 @@ const getRegexForCommand = (index: number): RegExp =>
   new RegExp(`${commandList[index].id}\\s+${commandList[index].description}`)
 
 describe('commands', () => {
-  test
-    .loadConfig({root: process.cwd()})
-    .stdout()
-    .command(['commands'])
-    .it('runs commands', (ctx) => {
-      expect(ctx.stdout).to.include('commands list all the commands')
-      expect(ctx.stdout).to.include('help     Display help for oclif-example.')
-    })
+  let sandbox: SinonSandbox
 
-  test
-    .stub(Commands.prototype, 'getCommands', (stub) => stub.returns(commandList))
-    .stdout()
-    .command(['commands', '--hidden'])
-    .it('runs commands --hidden', (ctx) => {
-      expect(ctx.stdout).to.match(getRegexForCommand(0))
-      expect(ctx.stdout).to.match(getRegexForCommand(1))
-      expect(ctx.stdout).to.not.match(getRegexForCommand(2))
-      expect(ctx.stdout).to.match(getRegexForCommand(3))
-      expect(ctx.stdout).to.match(getRegexForCommand(4))
-    })
+  beforeEach(() => {
+    sandbox = createSandbox()
+  })
 
-  test
-    .stub(Commands.prototype, 'getCommands', (stub) => stub.returns(commandList))
-    .stdout()
-    .command(['commands', '--filter=Command=^topic'])
-    .it('runs commands --filter="Command=^topic"', (ctx) => {
-      expect(ctx.stdout).to.match(getRegexForCommand(0))
-      expect(ctx.stdout).not.to.match(getRegexForCommand(1))
-      expect(ctx.stdout).to.not.match(getRegexForCommand(2))
-      expect(ctx.stdout).to.match(getRegexForCommand(3))
-      expect(ctx.stdout).to.not.match(getRegexForCommand(4))
-    })
+  afterEach(() => {
+    sandbox.restore()
+  })
 
-  test
-    .stub(Commands.prototype, 'getCommands', (stub) => stub.returns(commandList))
-    .stdout()
-    .command(['commands', '--filter=Plugin=anothertest'])
-    .it('runs commands --filter="Plugin=anothertest"', (ctx) => {
-      expect(ctx.stdout).to.not.match(getRegexForCommand(0))
-      expect(ctx.stdout).to.match(getRegexForCommand(1))
-      expect(ctx.stdout).to.not.match(getRegexForCommand(2))
-      expect(ctx.stdout).to.not.match(getRegexForCommand(3))
-      expect(ctx.stdout).to.not.match(getRegexForCommand(4))
+  it('prints commands', async () => {
+    const {stdout} = await runCommand(['commands'], {
+      root: process.cwd(),
     })
+    expect(stdout).to.include('commands list all the commands')
+    expect(stdout).to.include('help     Display help for oclif-example.')
+  })
 
-  test
-    .stub(Commands.prototype, 'getCommands', (stub) => stub.returns(commandList))
-    .stdout()
-    .command(['commands', '--filter=Command=anothertopic:subtopic:command'])
-    .it('runs commands --filter="Command=anothertopic:subtopic:command"', (ctx) => {
-      expect(ctx.stdout).to.not.match(getRegexForCommand(0))
-      expect(ctx.stdout).to.match(getRegexForCommand(1))
-      expect(ctx.stdout).to.not.match(getRegexForCommand(2))
-      expect(ctx.stdout).to.not.match(getRegexForCommand(3))
-      expect(ctx.stdout).to.not.match(getRegexForCommand(4))
-    })
+  it('runs commands --hidden', async () => {
+    // @ts-expect-error type mismatch
+    sandbox.stub(Commands.prototype, 'getCommands').returns(commandList)
+    const {stdout} = await runCommand(['commands', '--hidden'])
+    expect(stdout).to.match(getRegexForCommand(0))
+    expect(stdout).to.match(getRegexForCommand(1))
+    expect(stdout).to.not.match(getRegexForCommand(2))
+    expect(stdout).to.match(getRegexForCommand(3))
+    expect(stdout).to.match(getRegexForCommand(4))
+  })
 
-  test
-    .stub(Commands.prototype, 'getCommands', (stub) => stub.returns(commandList))
-    .stdout()
-    .command(['commands', '--columns=Command', '--no-header'])
-    .it('runs commands --filter=Command=', (ctx) => {
-      expect(ctx.stdout).to.match(/ anothertopic:subtopic:command\s+/)
-      expect(ctx.stdout).to.match(/ topic:subtopic:command\s+/)
-      expect(ctx.stdout).to.match(/ topic:subtopic:dep\s+/)
-      expect(ctx.stdout).to.not.include('topic:subtopic:deprecated')
-    })
+  it('runs commands --filter="Command=^topic"', async () => {
+    // @ts-expect-error type mismatch
+    sandbox.stub(Commands.prototype, 'getCommands').returns(commandList)
+    const {stdout} = await runCommand(['commands', '--filter=Command=^topic'])
+    expect(stdout).to.match(getRegexForCommand(0))
+    expect(stdout).not.to.match(getRegexForCommand(1))
+    expect(stdout).to.not.match(getRegexForCommand(2))
+    expect(stdout).to.match(getRegexForCommand(3))
+    expect(stdout).to.not.match(getRegexForCommand(4))
+  })
 
-  test
-    .stub(Commands.prototype, 'getCommands', (stub) => stub.returns(commandList))
-    .stdout()
-    .command(['commands', '--filter=Command=subtopic:command'])
-    .it('runs commands --filter"=Command=subtopic:command"', (ctx) => {
-      expect(ctx.stdout).to.equal(
-        ' Command                       Summary                         \n' +
-          ' ───────────────────────────── ─────────────────────────────── \n' +
-          ' anothertopic:subtopic:command another super good test command \n' +
-          ' topic:subtopic:command        super good test command         \n',
-      )
-    })
+  it('runs commands --filter="Plugin=anothertest"', async () => {
+    // @ts-expect-error type mismatch
+    sandbox.stub(Commands.prototype, 'getCommands').returns(commandList)
+    const {stdout} = await runCommand(['commands', '--filter=Plugin=anothertest'])
+    expect(stdout).to.not.match(getRegexForCommand(0))
+    expect(stdout).to.match(getRegexForCommand(1))
+    expect(stdout).to.not.match(getRegexForCommand(2))
+    expect(stdout).to.not.match(getRegexForCommand(3))
+    expect(stdout).to.not.match(getRegexForCommand(4))
+  })
 
-  test
-    .stub(Commands.prototype, 'getCommands', (stub) => stub.returns(commandList))
-    .stdout()
-    .command(['commands', '--filter=Command=^topic:subtopic:command'])
-    .it('runs commands --filter"=Command=^topic:subtopic:command"', (ctx) => {
-      expect(ctx.stdout).to.equal(
-        ' Command                Summary                 \n' +
-          ' ────────────────────── ─────────────────────── \n' +
-          ' topic:subtopic:command super good test command \n',
-      )
-    })
+  it('runs commands --filter="Command=anothertopic:subtopic:command"', async () => {
+    // @ts-expect-error type mismatch
+    sandbox.stub(Commands.prototype, 'getCommands').returns(commandList)
+    const {stdout} = await runCommand(['commands', '--filter=Command=anothertopic:subtopic:command'])
+    expect(stdout).to.not.match(getRegexForCommand(0))
+    expect(stdout).to.match(getRegexForCommand(1))
+    expect(stdout).to.not.match(getRegexForCommand(2))
+    expect(stdout).to.not.match(getRegexForCommand(3))
+    expect(stdout).to.not.match(getRegexForCommand(4))
+  })
 
-  test
-    .stub(Commands.prototype, 'getCommands', (stub) => stub.returns(commandList))
-    .stdout()
-    .command(['commands', '--json'])
-    .it('runs commands --json', (ctx: {stdout: string}) => {
-      const commands = JSON.parse(ctx.stdout)
-      expect(commands[0].id).to.equal('anothertopic:subtopic:command')
-      expect(commands[0].testCustomProperty).to.equal('test')
-      expect(commands[0].anotherCustomProperty[0]).to.equal(5)
-      expect(commands[0].anotherCustomProperty[1]).to.deep.equal({foo: 'bar'})
-      expect(commands[0]).to.not.have.property('circularObj')
-      expect(commands[1].id).to.equal('topic:subtopic:command')
-      expect(commands[1].testCustomProperty).to.equal('test')
-      expect(commands[1].anotherCustomProperty).to.equal(undefined)
-    })
+  it('runs commands --filter=Command=', async () => {
+    // @ts-expect-error type mismatch
+    sandbox.stub(Commands.prototype, 'getCommands').returns(commandList)
+    const {stdout} = await runCommand(['commands', '--columns=Command', '--no-header'])
+    expect(stdout).to.match(/ anothertopic:subtopic:command\s+/)
+    expect(stdout).to.match(/ topic:subtopic:command\s+/)
+    expect(stdout).to.match(/ topic:subtopic:dep\s+/)
+    expect(stdout).to.not.include('topic:subtopic:deprecated')
+  })
 
-  test
-    .stub(Commands.prototype, 'getCommands', (stub) => stub.returns(commandList))
-    .stdout()
-    .command(['commands'])
-    .it('hides deprecated commands', (ctx: {stdout: string}) => {
-      expect(ctx.stdout).to.not.include('topic:subtopic:deprecated')
-    })
+  it('runs commands --filter"=Command=subtopic:command"', async () => {
+    // @ts-expect-error type mismatch
+    sandbox.stub(Commands.prototype, 'getCommands').returns(commandList)
+    const {stdout} = await runCommand(['commands', '--filter=Command=subtopic:command'])
+    expect(stdout).to.equal(
+      ' Command                       Summary                         \n' +
+        ' ───────────────────────────── ─────────────────────────────── \n' +
+        ' anothertopic:subtopic:command another super good test command \n' +
+        ' topic:subtopic:command        super good test command         \n',
+    )
+  })
 
-  test
-    .stub(Commands.prototype, 'getCommands', (stub) => stub.returns(commandList))
-    .stdout()
-    .command(['commands'])
-    .it('hides deprecated aliases', (ctx: {stdout: string}) => {
-      expect(ctx.stdout).to.not.include('topic:subtopic:deprecated-alias')
-    })
+  it('runs commands --filter"=Command=^topic:subtopic:command"', async () => {
+    // @ts-expect-error type mismatch
+    sandbox.stub(Commands.prototype, 'getCommands').returns(commandList)
+    const {stdout} = await runCommand(['commands', '--filter=Command=^topic:subtopic:command'])
+    expect(stdout).to.equal(
+      ' Command                Summary                 \n' +
+        ' ────────────────────── ─────────────────────── \n' +
+        ' topic:subtopic:command super good test command \n',
+    )
+  })
 
-  test
-    .stub(Commands.prototype, 'getCommands', (stub) => stub.returns(commandList))
-    .stdout()
-    .command(['commands', '--deprecated'])
-    .it('shows deprecated commands when asked', (ctx: {stdout: string}) => {
-      expect(ctx.stdout).to.match(getRegexForCommand(0))
-      expect(ctx.stdout).to.match(getRegexForCommand(1))
-      expect(ctx.stdout).to.match(getRegexForCommand(2))
-      expect(ctx.stdout).to.match(getRegexForCommand(3))
-      expect(ctx.stdout).to.not.match(getRegexForCommand(4))
-    })
+  it('runs commands --json', async () => {
+    // @ts-expect-error type mismatch
+    sandbox.stub(Commands.prototype, 'getCommands').returns(commandList)
+    const {stdout} = await runCommand(['commands', '--json'])
+    const commands = JSON.parse(stdout)
+    expect(commands[0].id).to.equal('anothertopic:subtopic:command')
+    expect(commands[0].testCustomProperty).to.equal('test')
+    expect(commands[0].anotherCustomProperty[0]).to.equal(5)
+    expect(commands[0].anotherCustomProperty[1]).to.deep.equal({foo: 'bar'})
+    expect(commands[0]).to.not.have.property('circularObj')
+    expect(commands[1].id).to.equal('topic:subtopic:command')
+    expect(commands[1].testCustomProperty).to.equal('test')
+    expect(commands[1].anotherCustomProperty).to.equal(undefined)
+  })
+
+  it('hides deprecated commands', async () => {
+    // @ts-expect-error type mismatch
+    sandbox.stub(Commands.prototype, 'getCommands').returns(commandList)
+    const {stdout} = await runCommand(['commands'])
+    expect(stdout).to.not.include('topic:subtopic:deprecated')
+  })
+
+  it('hides deprecated aliases', async () => {
+    // @ts-expect-error type mismatch
+    sandbox.stub(Commands.prototype, 'getCommands').returns(commandList)
+    const {stdout} = await runCommand(['commands'])
+    expect(stdout).to.not.include('topic:subtopic:deprecated-alias')
+  })
+
+  it('shows deprecated commands when asked', async () => {
+    // @ts-expect-error type mismatch
+    sandbox.stub(Commands.prototype, 'getCommands').returns(commandList)
+    const {stdout} = await runCommand(['commands', '--deprecated'])
+    expect(stdout).to.not.include('topic:subtopic:deprecated-alias')
+    expect(stdout).to.match(getRegexForCommand(0))
+    expect(stdout).to.match(getRegexForCommand(1))
+    expect(stdout).to.match(getRegexForCommand(2))
+    expect(stdout).to.match(getRegexForCommand(3))
+    expect(stdout).to.not.match(getRegexForCommand(4))
+  })
 })
